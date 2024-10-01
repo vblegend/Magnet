9df3026c1e2e9549a61b7b15d27278aaf272771e
root <root@example.com>
2024-10-02 00:04:10 +0800

    1

--- Scripts/ScriptExample.cs
+++ Scripts/ScriptExample.cs
@@ -1,25 +1,25 @@
 ﻿using App.Core;
 using Magnet.Core;
+using System;
 
 
-namespace Scripts
+
+// A usable script must meet three requirements.
+// 1. The access must be public
+// 2. The Script Attribute must be marked
+// 3. The BaseScript class must be inherited
+
+[Script(nameof(ScriptExample))]
+public class ScriptExample : BaseScript
 {
-    // A usable script must meet three requirements.
-    // 1. The access must be public
-    // 2. The Script Attribute must be marked
-    // 3. The BaseScript class must be inherited
 
-    [Script(nameof(ScriptExample))] 
-    public class ScriptExample : BaseScript
+    [Function("Hello")]
+    public void Hello(String name)
     {
-
-        [Function("Hello")]
-        public void Hello(String name)
-        {
-            Console.WriteLine($"Hello {name}!");
-        }
-
-
-
+        Console.WriteLine($"Hello {name}!");
     }
+
+
+
 }
+
